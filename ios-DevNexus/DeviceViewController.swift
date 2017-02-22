10bb374c78b765a5922174b667b6223f7db39c2d
mjaeckel <marlene.jaeckel@gmail.com>
2017-02-22 00:06:10 -0500

    Start/stop accelerometer

--- ios-DevNexus/DeviceViewController.swift
+++ ios-DevNexus/DeviceViewController.swift
@@ -12,8 +12,16 @@ import MetaWear
 
 class DeviceViewController: UIViewController {
   @IBOutlet weak var deviceStatus: UILabel!
+  @IBOutlet weak var startAccelerometer: UIButton!
+  @IBOutlet weak var stopAccelerometer: UIButton!
+  @IBOutlet weak var stepCount: UILabel!
   
   var device: MBLMetaWear!
+  
+  var accelerometerBMI160: MBLAccelerometerBMI160!
+  var stepEvent: MBLEvent<MBLNumericData>!
+  var accelerometerData: NSMutableArray = NSMutableArray(capacity: 1000)
+  var steps: Int = 0
   
   override func viewWillAppear(_ animated: Bool) {
     super.viewWillAppear(animated)
@@ -21,6 +29,12 @@ class DeviceViewController: UIViewController {
     device.addObserver(self, forKeyPath: "state", options: NSKeyValueObservingOptions.new, context: nil)
     device.connectAsync().success { _ in
       NSLog("We are connected!")
+    }
+    
+    if device.accelerometer is MBLAccelerometerBMI160 {
+      accelerometerBMI160 = device.accelerometer as! MBLAccelerometerBMI160
+      accelerometerBMI160.sampleFrequency = 100
+      accelerometerBMI160.fullScaleRange = MBLAccelerometerBoschRange.range4G
     }
   }
   
@@ -46,5 +60,24 @@ class DeviceViewController: UIViewController {
         self.deviceStatus.text = "Discovery"
       }
     }
+  }
+  
+  @IBAction func startAccelerometer(sender: UIButton) {
+    if accelerometerBMI160 != nil {
+      self.stepEvent = accelerometerBMI160.stepEvent
+      stepEvent.startNotificationsAsync {(data: AnyObject?, error: Error?) -> Void in
+        if let steps = data as? MBLNumericData {
+          self.accelerometerData.add(steps)
+          self.steps += 1
+          NSLog("Total steps taken: %d", self.steps)
+          self.stepCount.text = NSString(format: "%d", self.steps) as String
+        }
+      }
+    }
+  }
+  
+  @IBAction func stopAccelerometer(sender: UIButton) {
+    stepEvent.stopNotificationsAsync()
+    self.stepCount.text = "---"
   }
 }
